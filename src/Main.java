4fd63d9f914ff072e01a8656e690edabb4dc277e
c_der <c_der@ZAADUM>
2018-09-22 21:10:02 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,25 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Box box = new Box(10);
+        Box littlebox = new Box(10);
 
-        box.add( new Book("Fedor Dostojevski", "Crime and Punishment", 2) ) ;
-        box.add( new Book("Robert Martin", "Clean Code", 1) );
-        box.add( new Book("Kent Beck", "Test Driven Development", 0.7) );
+        littlebox.add( new Book("Fedor Dostojevski", "Crime and Punishment", 2) ) ;
+        littlebox.add( new Book("Robert Martin", "Clean Code", 1) );
+        littlebox.add( new Book("Kent Beck", "Test Driven Development", 0.7) );
 
-        box.add( new CD("Pink Floyd", "Dark Side of the Moon", 1973) );
-        box.add( new CD("Wigwam", "Nuclear Nightclub", 1975) );
-        box.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
+        littlebox.add( new CD("Pink Floyd", "Dark Side of the Moon", 1973) );
+        littlebox.add( new CD("Wigwam", "Nuclear Nightclub", 1975) );
+        littlebox.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
 
-        System.out.println( box );
+        System.out.println( littlebox );
+        
+        Box bigBox = new Box (15);
+        
+        for(Integer i=0; i<4; i++) {
+        	bigBox.add(littlebox);
+        }
+        
+        System.out.println(bigBox);
     }
     
 }
